e0b9494dccae09b45bb2dfa8dbb4e4fb904e75b4
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-08-01 21:51:33 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -754,4 +754,5 @@ if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
     pbot.start()
-    asyncio.run(main())
+    main()
+#    asyncio.run(main())
